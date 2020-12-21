c8e0f02182ac2412115d610564c84f71517ac298
prometheus <martin1.bugar@gmail.com>
2020-12-21 10:45:35 +0100

    Dependency

---
+++ src/main/java/com/martyx/springbootspringsecurityjwtauthentication/SpringBootSpringSecurityJwtAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.martyx.springbootspringsecurityjwtauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringSecurityJwtAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpringSecurityJwtAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martyx/springbootspringsecurityjwtauthentication/SpringBootSpringSecurityJwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martyx.springbootspringsecurityjwtauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSpringSecurityJwtAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
